955011112a43c87c8532952648e4341e0770d998
Amos Alagbe <sunboy005@gmail.com>
2022-04-01 18:14:14 +0100

    josey-chatbox-css-updates

--- includes/general/chatwidget.php
+++ includes/general/chatwidget.php
@@ -13,28 +13,53 @@
     <div id="screen">
         <div id="chat-header"><i class="fa fa-user-o"></i>  &nbsp; MSME Chat Support &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <i class="clickable fa fa-times" aria-hidden="true"></i></div>
         <div id="messageDisplaySection">
-            <!-- bot messages -->
-            <div class="chat botMessages">Hello</div>
-
-            <!-- bot messages -->
+            <!--bot messages
+            <div class="chat botMessages">Hello, How may I help you</div>
+           
             <div id="messagesContainer">
-                <div class="chat userMessages">Hi</div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-8" id="chat-input">
-                <input type="text" id="chat-input-field" placeholder="Type your message here...">
-            </div>
-        </div>
-        <div id="voiceInput">
-        <button id="voiceInput" name="voiceInput" class="btn btn-primary"><i class="fa fa-microphone"></i></button>
-        </div>
+                <div class="chat userMessages">Hi, am looking for cards</div>
+            </div>-->
+            
+        </div>       
         <!-- chat input field -->
         <div id="userInput">
-            <input type="text" id="messages" autocapitalize="none" autocomplete="OFF" placeholder="Type your message here..." requred>
-            <button id="send" name="send" class="btn btn-primary"><i class="fa fa-send"></i></button>
+            <input type="text" name="messages"  id="messages" autocapitalize="none" autocomplete="OFF" placeholder="Type your message here..." required>
+            <button type="submit" id="send" name="send" class="btn btn-primary" tooltip="Send"><i class="fa fa-send"></i></button>            
+            <button id="voiceInput" name="voice" class="btn btn-info" tooltip="Voice to text"><i class="fa fa-microphone"></i></button>
         </div>
     </div>
 </div>  
 <!-- Chat-Bot End -->
+<!-- JQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
+<!-- JQuery Start -->
+<script>
+$(document).ready(function(){
+    $("#messages").on("keyup", function(){
+        if($("#messages").val()){
+            $("#send").css("display", "block");
+        }else{
+            $("#send").css("display", "none");
+        }
+    });
+});   
+//Send Button Click
+$("#send").click(function(e){
+     $userMessage = $("#messages").val();
+     $appendUserMessage = "<div class='chat userMessages'>"+$userMessage+"</div>";
+      $("#messageDisplaySection").append($appendUserMessage);
+      //ajax start
+    $.ajax({
+        url: "chatbot.php",
+        type: "POST",
+        data: {messageValue: userMessage},
+        //response
+        success: function(data){
+            $("#messagesContainer").append("<div class='chat botMessages'>"+data+"</div>");
+        }        
+    });
+    $("#messages").val("");
+    $("#send").css("display", "none");
+});
+</script>
